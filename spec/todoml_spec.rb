caa59fac46f5307b1bb0bdf6d8925c919acf110f
Leo Lou <louyuhong@gmail.com>
2013-03-13 14:09:46 +0800

    Try with ordered hash

--- spec/todoml_spec.rb
+++ spec/todoml_spec.rb
@@ -2,7 +2,15 @@ require 'spec_helper'
 require 'yaml'
 describe Todoml do
   it "should parse simple yaml" do
-    data = YAML.load_file "./spec/data/simple.yaml"
-    p data
+    YAML.load_file "./spec/data/simple.yaml"
+  end
+
+  describe "retrieve data" do
+    before :each do
+      @data = YAML.load_file "./spec/data/simple.yaml"
+    end
+    it "should retrieve velocity" do
+      @data['velocity'].should eq 10
+    end
   end
 end
