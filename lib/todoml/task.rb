0af8998ff14450b9b99e3c9c4c5de2b03b57123b
Leo Lou <louyuhong@gmail.com>
2013-03-13 14:57:59 +0800

    Refactor task initializer to support single string

--- lib/todoml/task.rb
+++ lib/todoml/task.rb
@@ -2,9 +2,18 @@ module Todoml
   class Task
     attr_accessor :name, :point
 
-    def initialize(params = {})
-      @name = params.fetch :name
-      @point = params.fetch :point, 1
+    def initialize(task_raw)
+      if task_raw.is_a? String
+        @name = task_raw
+        @point = 1
+      elsif task_raw.is_a? Array and task_raw.length == 2 and
+        task_raw[0].is_a? String and task_raw[1].is_a? Numeric
+        @name = task_raw[0]
+        @point = task_raw[1]
+      else 
+        raise TypeError
+      end
+
     end
   end
 end
--- spec/todoml_spec.rb
+++ spec/todoml_spec.rb
@@ -3,14 +3,16 @@ require 'yaml'
 describe Todoml do
   describe "task" do
     it "should be initialized with name and point" do
-      task = Todoml::Task.new({name: 'Task 1', point: 3})
+      task_raw = ["Task 1", 3]
+      task = Todoml::Task.new(task_raw)
       task.name.should eq 'Task 1'
       task.point.should eq 3
     end
 
-    it "should have a default value of point 1" do
-      task = Todoml::Task.new({name: 'Task lazy'})
-      task.name.should eq 'Task lazy'
+    it "should handle tasks in a single string with a default point 1" do
+      task_raw = "Task single string"
+      task = Todoml::Task.new(task_raw)
+      task.name.should eq 'Task single string'
       task.point.should eq 1 
     end
   end
@@ -19,7 +21,7 @@ describe Todoml do
     YAML.load_file "./spec/data/simple.yaml"
   end
 
-  describe "retrieve data" do
+  describe "simple data" do
     before :each do
       @data = YAML.load_file "./spec/data/simple.yaml"
     end
@@ -30,8 +32,28 @@ describe Todoml do
 
     it "should retreve tasks" do
       @data['CURRENT'].count.should eq 1
-      task = @data['CURRENT'].first
-      task = Todoml::Task.new({name: task[0], point: task[1]})
+      task_raw = @data['CURRENT'].first
+      task = Todoml::Task.new(task_raw)
+      task.name.should eq "User can read learn you some erlang"
+      task.point.should eq 3
+    end
+  end
+
+  describe "case 1" do
+    before :each do
+      @data = YAML.load_file "./spec/data/case1.yaml"
+    end
+
+    it "should parse unfinished tasks and convert them to a list of Tasks" do
+      unfinished_tasks = @data['CURRENT'] + @data['TODO']
+      unfinished_tasks.reject! { |task_raw| task_raw.nil? }
+      unfinished_tasks.map! { |task_raw| 
+        Todoml::Task.new(task_raw)
+      }
+
+      unfinished_tasks.count.should eq 11
+
+      task = unfinished_tasks.first
       task.name.should eq "User can read learn you some erlang"
       task.point.should eq 3
     end
