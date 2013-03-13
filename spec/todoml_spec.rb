16cbc9b093c52b86b97765ec913934dfacc1f3a0
Leo Lou <louyuhong@gmail.com>
2013-03-13 14:13:31 +0800

    Add tests for retrieving tasks

--- spec/todoml_spec.rb
+++ spec/todoml_spec.rb
@@ -9,8 +9,16 @@ describe Todoml do
     before :each do
       @data = YAML.load_file "./spec/data/simple.yaml"
     end
+
     it "should retrieve velocity" do
       @data['velocity'].should eq 10
+    end
+
+    it "should retreve tasks" do
+      @data['CURRENT'].count.should eq 1
+      task = @data['CURRENT'].first
+      task[0].should eq "User can read learn you some erlang"
+      task[1].should eq 3
     end
   end
 end
