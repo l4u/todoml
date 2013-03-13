d695b2f1d25df8076ffcec3abbdd001ec91ab307
Leo Lou <louyuhong@gmail.com>
2013-03-13 17:47:41 +0800

    Refactor to_yaml_simple

--- lib/todoml/tasks.rb
+++ lib/todoml/tasks.rb
@@ -2,18 +2,16 @@ module Todoml
   class Tasks < Hash
     attr_accessor :other_yaml
     def to_yaml_simple
-      other_yaml.to_yaml + 
-      "CURRENT:\n" +
-        self['CURRENT'].map { |task| "    - " + task.to_yaml_simple}.join("\n") +
-        "\n" +
-        "\n" +
-        "TODO:\n" +
-        self['TODO'].map { |task| "    - " + task.to_yaml_simple}.join("\n") +
-        "\n" +
-        "\n" +
-        "FINISHED:\n" +
-        self['FINISHED'].map { |task| "    - " + task.to_yaml_simple}.join("\n") +
-        "\n"
+      results = other_yaml.to_yaml
+      ['CURRENT', 'TODO', 'FINISHED'].each do |group|
+        results << "#{group}:\n"
+        results << self[group].map { |task| 
+          "    - " + task.to_yaml_simple
+        }.join("\n")
+        results << "\n"
+        results << "\n" if group != 'FINISHED'
+      end
+      results
     end
   end
 end
